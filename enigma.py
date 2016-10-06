ddc692948bd20fa15d9b58660fb93004122c0c21
ecdani <ec.dani@gmail.com>
2016-10-06 18:19:56 +0200

    V2 mejorada enigma

--- enigma.py
+++ enigma.py
@@ -6,8 +6,8 @@ class Enigma:
     rfb = list('YRUHQSLDPXNGOKMIEBFZCWVJAT')
     clv = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     key = list('KWC')
-    msj = list('PULJEQTVXYVTDOM')
-    
+    msj = list('CALIDADDESISTEMASSOFTWAREFRANCISCOMONTEROESCUELASUPERIORDEINGENIERIAINFORMATICA')
+
     # Estado interno
     rk1, rk2, rk3 = 0,0,0
     def putkey(self):
@@ -15,18 +15,16 @@ class Enigma:
         self.rk2 = self.abc.index(self.key[1])
         self.rk3 = self.abc.index(self.key[2])
 
-    def rotor(self,ch,abc,rot,rk):
-        s = rot[(abc.index(ch)+rk) % 26] # Decode
-        s = abc[(abc.index(s)-rk) % 26]
-        return s
+    def rotor(self,ch,rot,rk):
+        s = rot[(self.abc.index(ch)+rk) % 26]
+        return self.abc[(self.abc.index(s)-rk) % 26]
 
-    def rotorinv(self,ch,abc,rot,rk):
-        s = abc[(abc.index(ch)+rk) % 26]
-        s = abc[(rot.index(s)-rk) % 26] # Decode
-        return s
+    def rotorinv(self,ch,rot,rk):
+        s = self.abc[(self.abc.index(ch)+rk) % 26]
+        return self.abc[(rot.index(s)-rk) % 26]
 
     def clav(self,ch,abc,clv):
-        return self.clv[self.abc.index(ch)]
+        return clv[abc.index(ch)]
 
     def rotar(self):
         self.rk3 += 1
@@ -37,31 +35,27 @@ class Enigma:
             self.rk2 = 0
             self.rk1 += 1
 
-    def code(self,ch):
+    def encode(self,ch):
         ch = self.clav(ch,self.abc,self.clv)
         self.rotar()
-        ch = self.rotor(ch,self.abc,self.r3,self.rk3)
-        ch = self.rotor(ch,self.abc,self.r2,self.rk2)
-        ch = self.rotor(ch,self.abc,self.r1,self.rk1)
+        ch = self.rotor(ch,self.r3,self.rk3)
+        ch = self.rotor(ch,self.r2,self.rk2)
+        ch = self.rotor(ch,self.r1,self.rk1)
         ch = self.reflector(ch)
-        ch = self.rotorinv(ch,self.abc,self.r1,self.rk1)
-        ch = self.rotorinv(ch,self.abc,self.r2,self.rk2)
-        ch = self.rotorinv(ch,self.abc,self.r3,self.rk3)
+        ch = self.rotorinv(ch,self.r1,self.rk1)
+        ch = self.rotorinv(ch,self.r2,self.rk2)
+        ch = self.rotorinv(ch,self.r3,self.rk3)
         ch = self.clav(ch,self.clv,self.abc)
         return ch
 
     def reflector(self,ch):
-        self.inv = True
-        s = self.rfb[self.abc.index(ch)] # Decode
-        return s
+        return self.rfb[self.abc.index(ch)]
 
     def translate(self):
         self.putkey()
         cod = list()
         for ch in self.msj:
-            s = self.code(ch)
-            cod.append(s)
+            cod.append(self.encode(ch))
         print(cod)
 
-e = Enigma()
-e.translate()
+Enigma().translate()
