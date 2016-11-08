e59c0ee8ffee47ee6e90c4c35b80607c80b5b8c9
Daniel Espinosa <ec.dani@gmail.com>
2016-11-08 12:31:35 +0100

    ataque sobre diffie

--- diffie.py
+++ diffie.py
@@ -13,15 +13,35 @@ def ExpM (n,x,p):
             final *= n
     return (final % p)
 
+def ataque (n,p, alice, bob):
+    i = 1
+    z = None
+    while True:
+        deco = ExpM(n,i,p)
+        if deco == alice:
+            return ExpM(bob,i,p)
+        elif deco == bob:
+            return ExpM(alice,i,p)
+        i += 1
+
 # Ejemplo https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange
-alice =  ExpM(5,6,23) # 8
+
+n = 65537#5
+p = 1372933 #23
+xa = 5858999999#6
+xb = 7888899999 #15
+
+alice =  ExpM(n,xa,p) # 8
 print('Alice: %i' % alice)
 
-bob = ExpM(5,15,23)
+bob = ExpM(n,xb,p)
 print('Bob: %i' % bob)
 
-alice_s = ExpM(bob,6,23)
+alice_s = ExpM(bob,xa,p)
 print('Alice s: %i' % alice_s)
 
-bob_s = ExpM(alice,15,23)
+bob_s = ExpM(alice,xb,p)
 print('Bob s: %i' % bob_s)
+
+sDescifrada = ataque(n,p,alice,bob)
+print('Clave descifrada: %i' % sDescifrada)
